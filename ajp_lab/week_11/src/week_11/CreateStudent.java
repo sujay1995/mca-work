2e14c16fcf61443dfcc5cc7f06356fd7e7b035dc
sujay1995 <sujayssrvm@gmail.com>
2019-11-21 15:32:09 +0530

    nov21re-commit

--- ajp_lab/week_11/src/week_11/CreateStudent.java
+++ ajp_lab/week_11/src/week_11/CreateStudent.java
@@ -11,27 +11,15 @@ import java.sql.SQLException;
 import week_11.Connectivity;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class CreateStudent
- */
+@WebServlet("/createStudent")
 public class CreateStudent extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CreateStudent() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
@@ -65,14 +53,6 @@ public class CreateStudent extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}		 
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
